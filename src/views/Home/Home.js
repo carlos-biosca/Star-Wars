140250dfb6c3daf80cb243b952e98e9214b0b099
Carlos <cbiosca82gmail.com>
2021-11-17 17:18:09 +0100

    cards section part1

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,26 @@
 import ImageSlider from "../../components/ImageSlider/ImageSlider";
 
+import card1 from '../../assets/images/cards/card1.webp'
+
+import './Home.css'
+
 export default function Home () {
   return (
     <>
       <ImageSlider />
+      <section className="cards">
+        <div className="card">
+          <div className="card__image">
+            <img src={card1} alt="" />
+          </div>
+          <div className="card__content">
+            <div className="card__decal"></div>
+            <h3 className="card__title">INSIDE STAR WARS: VISIONS: MEET THE SHIPS AND VEHICLES</h3>
+            {/* <div className="card__gradient"></div> */}
+          </div>
+          <div className="card__footer"></div>
+        </div>
+      </section>
     </>
   )
 }
