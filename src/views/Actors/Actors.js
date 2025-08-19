fbf232d4588f56f3838720abd951603b4f78afb7
Carlos <cbiosca82@gmail.com>
2025-08-19 21:04:43 +0200

    get actors from new url

--- src/views/Actors/Actors.js
+++ src/views/Actors/Actors.js
@@ -7,13 +7,11 @@ import Button from '../../components/Button/Button';
 import LoadingSpinner from '../../components/Spinner/Spinner'
 
 import retrieveActors from '../../logic/retrieve-actors'
-import getData from '../../utils/getData';
 
 import axios from 'axios'
 
 export default function Actors () {
   const [actorsList, setActorsList] = useState(null)
-  const [page, setPage] = useState()
 
   const sourceRef = useRef(axios.CancelToken.source())
 
@@ -21,25 +19,16 @@ export default function Actors () {
     const source = sourceRef.current
     const getActors = async () => {
       const res = await retrieveActors()
-      setActorsList(res.results)
-      setPage(res.next)
+      setActorsList(res)
     }
     getActors()
 
     return () => {
       if (source) source.cancel("Landing Component got unmounted");
       setActorsList([])
-      setPage()
     }
   }, [])
 
-  const handleAddActors = async () => {
-    if (page) {
-      const res = await getData(page)
-      setActorsList([...actorsList, ...res.results])
-      setPage(res.next)
-    }
-  }
 
   const handleScrollTop = () => {
     window.scrollTo(0, 0)
@@ -51,13 +40,7 @@ export default function Actors () {
         actorsList ? (
           <>
             <List list={actorsList} page={'actors'} />
-            {
-              actorsList.length < 82 ? (
-                <Button open={handleAddActors} text={'VIEW MORE'} name={'view__button'} />
-              ) : (
-                <Button open={handleScrollTop} text={'BACK TO TOP'} name={'view__button'} />
-              )
-            }
+            <Button open={handleScrollTop} text={'BACK TO TOP'} name={'view__button'} />
           </>
         ) : (
           <LoadingSpinner />
